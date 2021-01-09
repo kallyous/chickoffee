a2324630af3fcaa8d5cc74b8ed6d6f25e585bcf6
Lucas Carvalho <lcf@ic.ufal.br>
2021-01-09 18:05:51 -0300

    Implementa armadilha de espetos.

---
+++ Assets/Prefabs/Player/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+
+    public float harmInterval = 1f;
+    private float nextHarmTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        print("Pinto saudável, " + currentHealth + " HP");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            print("O pinto foi abatido! T_T");
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (Time.time >= nextHarmTime)
+        {
+            currentHealth -= damage;
+            nextHarmTime = Time.time + harmInterval;
+            print("Pinto ferido! " + currentHealth + " HP");
+        }
+    }
+}
---
+++ Assets/Prefabs/Traps/Trap_Spike.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_Spike : MonoBehaviour
+{
+    public int damage = 1;
+
+	// Interno do Unity, dispara quando algo colide com um IsTrigger.
+	void OnTriggerEnter2D (Collider2D hitInfo)
+	{
+		Player player = hitInfo.GetComponent<Player>();
+		if (player != null)
+		{
+			player.TakeDamage(damage);
+		}
+	}
+}
