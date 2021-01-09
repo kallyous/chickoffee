bd4593dec8214dd32bde146600b687814c2e10e4
Lucas Carvalho <lcf@ic.ufal.br>
2021-01-09 11:53:00 -0300

    Adiciona café com sprite placeholder.

---
+++ Assets/Prefabs/Projectiles/Bullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 20f;
+	public int damage = 1;
+	public Rigidbody2D rb;
+	// public GameObject impactEffect;
+
+	// Use this for initialization
+	void Start () {
+		rb.velocity = transform.right * speed;
+	}
+
+	// Interno do Unity, dispara quando algo colide com um IsTrigger.
+	void OnTriggerEnter2D (Collider2D hitInfo)
+	{
+		// Enemy enemy = hitInfo.GetComponent<Enemy>();
+		// if (enemy != null)
+		// {
+		// 	enemy.TakeDamage(damage);
+		// }
+
+		// Instantiate(impactEffect, transform.position, transform.rotation);
+
+        GameObject obj = hitInfo.gameObject;
+		if (obj != null)
+		{
+			print(obj.name);
+		}
+
+		Destroy(gameObject);
+	}
+	
+}
