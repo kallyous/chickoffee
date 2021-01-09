563cc1a8c132e2087e130cf0d7092413a7ce55d1
Lucas Carvalho <lcf@ic.ufal.br>
2021-01-09 10:00:48 -0300

    Move o pinto.

--- Assets/Prefabs/Player/PlayerMovement.cs
+++ Assets/Prefabs/Player/PlayerMovement.cs
@@ -5,51 +5,37 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public CharacterController2D controller;
-	public Animator animator;
+	// public Animator animator;
 
 	public float runSpeed = 40f;
 
 	float horizontalMove = 0f;
 	bool jump = false;
-	bool crouch = false;
 
 	// Update is called once per frame
 	void Update () {
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+		// animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
 		if (Input.GetButtonDown("Jump"))
 		{
 			jump = true;
-			animator.SetBool("IsJumping", true);
-		}
-
-		if (Input.GetButtonDown("Crouch"))
-		{
-			crouch = true;
-		} else if (Input.GetButtonUp("Crouch"))
-		{
-			crouch = false;
+			// animator.SetBool("IsJumping", true);
 		}
 
 	}
 
 	public void OnLanding ()
 	{
-		animator.SetBool("IsJumping", false);
-	}
-
-	public void OnCrouching (bool isCrouching)
-	{
-		animator.SetBool("IsCrouching", isCrouching);
+		// animator.SetBool("IsJumping", false);
 	}
 
 	void FixedUpdate ()
 	{
 		// Move our character
-		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
 		jump = false;
 	}
 }
