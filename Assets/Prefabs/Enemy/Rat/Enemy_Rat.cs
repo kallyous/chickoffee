05eac2a21ec4095d6d75c041c16a923c709aa837
Lucas Carvalho <lcf@ic.ufal.br>
2021-01-10 00:56:10 -0300

    Detecção do player e perseguição no comportamento do rato

---
+++ Assets/Prefabs/Enemy/Rat/Enemy_Rat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Rat : MonoBehaviour
+{
+    Enemy self;
+    Animator animator;
+
+    public float attackRange = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        self = transform.parent.gameObject.GetComponent<Enemy>();
+        animator = transform.parent.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
