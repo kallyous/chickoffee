f5d9f7d03fd844333e935dc54acc9e95d351a67c
Lucas Carvalho <lcf@ic.ufal.br>
2021-01-10 08:19:26 -0300

    Ajusta projétil de café.

--- Assets/Prefabs/Projectiles/Bullet.cs
+++ Assets/Prefabs/Projectiles/Bullet.cs
@@ -7,20 +7,38 @@ public class Bullet : MonoBehaviour {
 	public float speed = 20f;
 	public int damage = 1;
 	public Rigidbody2D rb;
-	// public GameObject impactEffect;
 
+	public float duration = 1f;
+	private float dieTime;
+
+	public float harmCooldown = 0.6f;
+	private float nextHarm = 0f;
+	
 	// Use this for initialization
 	void Start () {
 		rb.velocity = transform.right * speed;
+		dieTime = Time.time + duration;
+	}
+
+	void Update()
+	{
+		if (Time.time >= dieTime)
+		{
+			Destroy(gameObject);
+		}
 	}
 
 	// Interno do Unity, dispara quando algo colide com um IsTrigger.
 	void OnTriggerEnter2D (Collider2D hitInfo)
 	{
-		Enemy enemy = hitInfo.GetComponent<Enemy>();
-		if (enemy != null)
+		if (Time.time >= nextHarm)
 		{
-			enemy.TakeDamage(damage);
+			Enemy enemy = hitInfo.GetComponent<Enemy>();
+			if (enemy != null)
+			{
+				enemy.TakeDamage(damage);
+				nextHarm = Time.time + harmCooldown;
+			}
 		}
 	}
 	
