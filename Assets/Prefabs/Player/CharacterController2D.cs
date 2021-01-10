abd19b5a65033bb4e3fcd7ea742322f94ed58be8
Lucas Carvalho <lcf@ic.ufal.br>
2021-01-10 08:45:05 -0300

    Ajusta mira do pinto.

--- Assets/Prefabs/Player/CharacterController2D.cs
+++ Assets/Prefabs/Player/CharacterController2D.cs
@@ -133,7 +133,7 @@ public class CharacterController2D : MonoBehaviour
 	}
 
 
-	private void Flip()
+	public void Flip()
 	{
 		// Switch the way the player is labelled as facing.
 		m_FacingRight = !m_FacingRight;
--- Assets/Prefabs/Projectiles/CoffeeWeapon.cs
+++ Assets/Prefabs/Projectiles/CoffeeWeapon.cs
@@ -14,31 +14,40 @@ public class CoffeeWeapon : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		// Pega posição do mouse na tela e converte na posição do espaço do jogo.
-		mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-
-		// Calcula vetor que descreve a direção que sai do firePoint para o mouse (Valei-me Geometria Analítica!!).
-		Vector2 aimDir = mousePosition - firePoint.position;
-		
-		// Inverte x se estiver olhando para esquerda
-		if (!controller.m_FacingRight)
-		{
-			aimDir.x = aimDir.x * -1;
-		}
-
-		// Usa arco-tangente pra calcular o angulo descrito pelo vetor direção, para girar o firePoint.
-		float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg; // Sim, é (y, x) sabe Deus pq !?
-
-		// Atualiza rotação de firePoint
-		firePoint.transform.eulerAngles = new Vector3(
-			firePoint.transform.eulerAngles.x,
-			firePoint.transform.eulerAngles.y,
-			angle
-		);
-
 		// Atira!
 		if (Input.GetButtonDown("Fire1"))
 		{
+			if (mousePosition.x < firePoint.position.x && controller.m_FacingRight)
+			{
+				controller.Flip();
+			}
+			else if (firePoint.position.x < mousePosition.x && !controller.m_FacingRight) {
+				controller.Flip();
+			}
+
+			// Pega posição do mouse na tela e converte na posição do espaço do jogo.
+			mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+			// Calcula vetor que descreve a direção que sai do firePoint para o mouse (Valei-me Geometria Analítica!!).
+			Vector2 aimDir = mousePosition - firePoint.position;
+
+			// Inverte x se estiver olhando para esquerda, exceto que o nome da variável está invertido no script.
+			// Semanticamente falando deveria ser !controller.m_FacingLeft mas foda-se...
+			if (!controller.m_FacingRight)
+			{
+				aimDir.x = aimDir.x * -1;
+			}
+
+			// Usa arco-tangente pra calcular o angulo descrito pelo vetor direção, para girar o firePoint.
+			float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg; // Sim, é (y, x) sabe Deus pq !?
+
+			// Atualiza rotação de firePoint
+			firePoint.transform.eulerAngles = new Vector3(
+				firePoint.transform.eulerAngles.x,
+				firePoint.transform.eulerAngles.y,
+				angle
+			);
+
 			Shoot();
 		}
 	}
