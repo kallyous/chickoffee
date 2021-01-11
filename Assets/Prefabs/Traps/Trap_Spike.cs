59e36fd54ef2a5a353d96a96a0e67f97f7878db6
Lucas Carvalho <lcf@ic.ufal.br>
2021-01-10 23:05:12 -0300

    Junta sprites

--- Assets/Prefabs/Traps/Trap_Spike.cs
+++ Assets/Prefabs/Traps/Trap_Spike.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Trap_Spike : MonoBehaviour
 {
     public int damage = 1;
+	public AudioPlayer hitSound;
 
 	// Interno do Unity, dispara quando algo colide com um IsTrigger.
 	void OnTriggerEnter2D (Collider2D hitInfo)
@@ -12,6 +13,7 @@ public class Trap_Spike : MonoBehaviour
 		Player player = hitInfo.GetComponent<Player>();
 		if (player != null)
 		{
+			hitSound.Play();
 			player.TakeDamage(damage);
 		}
 	}
