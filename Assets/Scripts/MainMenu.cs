d3934f1437e5dd77467ac585c152c315d223d60b
Lucas Carvalho <lcf@ic.ufal.br>
2021-01-11 19:56:01 -0300

    Corrige bug no tiro de café e alivia zona final por uma armadilha.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,11 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Credits()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
